002b27fd0853c8d318683eb7dd6cf5e9b3b891c0
willTinguely <thewillbob@gmail.com>
2016-12-23 19:18:06 -0600

    Init MVC/React hello world

---
+++ StudyShare/StudyShare/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudyShare.MvcApplication" Language="C#" %>
---
+++ StudyShare/StudyShare/Scripts/HelloWorld.jsx
@@ -0,0 +1,13 @@
+﻿var CommentBox = React.createClass({displayName: 'CommentBox',
+  render: function() {
+    return (
+      React.createElement('div', {className: "commentBox"},
+        "Hello, world!"
+      )
+    );
+  }
+});
+ReactDOM.render(
+  React.createElement(CommentBox, null),
+  document.getElementById('content')
+);
