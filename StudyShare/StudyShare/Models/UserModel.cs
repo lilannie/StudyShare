4e6e7c886785429451c96151f3dc213c738a3584
willTinguely <thewillbob@gmail.com>
2017-01-09 19:23:44 -0600

    added data model

---
+++ StudyShare/StudyShare/Models/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudyShare.Models
+{
+    public class UserModel
+    {
+        //Auto-Gen database ID
+        public int UserId { get; set; }
+ 
+        //basic user info
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get { return String.Format("{0} {1}", this.FirstName, this.LastName); } }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        //Time logging
+        public DateTime AccountCreatedOnDate { get; }
+        public DateTime LastLoggin { get; set; }
+        
+        //Account info
+        public bool isAdmin { get; set; }
+        public bool isTeacher { get; set; }
+
+
+    }
+}
